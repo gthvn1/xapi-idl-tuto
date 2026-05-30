045d7d74e3a61ac16642c46a64074e9616133785
Guillaume <gthvn1@gmail.com>
2026-05-30 15:02:59 +0200

    add an example for tcp client

---
+++ client_example/Readme.md
@@ -0,0 +1,1 @@
+socat TCP-LISTEN:8080,reuseaddr SYSTEM:'cat'
---
+++ client_example/client.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const net = std.Io.net;
+
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+    var bufreader: [1024]u8 = undefined;
+    var bufwriter: [1024]u8 = undefined;
+
+    const address = try net.IpAddress.parseIp4("127.0.0.1", 8080);
+    const s = try net.IpAddress.connect(
+        &address,
+        io,
+        net.IpAddress.ConnectOptions{
+            .mode = net.Socket.Mode.stream,
+        },
+    );
+
+    var stream_reader = net.Stream.reader(s, io, &bufreader);
+    const r = &stream_reader.interface;
+
+    var stream_writer = net.Stream.writer(s, io, &bufwriter);
+    const w = &stream_writer.interface;
+
+    try w.writeAll("Hello from client\n");
+    try w.flush();
+
+    var answer: [64]u8 = undefined;
+    var i: usize = 0;
+    while (i < answer.len) {
+        const byte_read = try r.readSliceShort(answer[i .. i + 1]);
+        if ((byte_read == 0) or (answer[i] == '\n')) break;
+        std.debug.print("one byte read: <{c}>", .{answer[i]});
+        i += byte_read;
+    }
+}
